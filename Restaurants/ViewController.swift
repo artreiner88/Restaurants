658b2d566f4cf95da742e5ac488ddad5b368c835
artreiner88 <artur.reiner88@gmail.com>
2024-05-10 21:35:28 +0200

    Initial Commit

---
+++ Restaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurants
+//
+//  Created by Artur Reiner on 10.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
